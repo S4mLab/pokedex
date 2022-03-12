1b189d740779f7fe1bbeb4b9161b6d04ef3cbf45
sam <samvbui1995@gmail.com>
2022-03-13 09:59:03 +1100

    learn about store

--- src/stores/pokemart.js
+++ src/stores/pokemart.js
@@ -1,6 +1,16 @@
-const axios = require('axios');
+import { writable } from 'svelte/store';
+import axios from 'axios';
+
+export let pokemon = writable([]);
 
 const fetchPokemons = async () => {
-	const pokemonsUrl = '';
-	const response = axios.post(pokemonsUrl);
+	const pokemonsUrl = 'https://pokeapi.co/api/v2/pokemon?limit=5';
+	try {
+		const response = await axios.get(pokemonsUrl);
+		const rawPokeObjsList = await response.data.results;
+	} catch (err) {
+		console.error(err);
+	}
 };
+
+fetchPokemons();
